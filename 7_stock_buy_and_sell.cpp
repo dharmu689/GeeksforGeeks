4378818e61037f724ed771cc9506bb0e2696a531
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-30 21:57:01 +0530

    Create 7_stock_buy_and_sell.cpp

---
+++ 7_stock_buy_and_sell.cpp
@@ -0,0 +1,58 @@
+// Question -https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/stock-buy-and-sell2615
+
+
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        // code here
+        int res = 0;
+  
+    // Keep on adding the difference between
+    // adjacent when the prices a
+    for (int i = 1; i < prices.size(); i++) 
+    
+    {
+        if (prices[i] > prices[i - 1]) 
+        {
+            res += prices[i] - prices[i - 1];
+        }    
+    }
+    
+    return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        Solution ob;
+        int res = ob.maximumProfit(arr);
+        cout << res;
+
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
