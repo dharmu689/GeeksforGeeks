5a8fb66aef67fc52fb1152c0a895b6bb8d9cafa8
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-01-28 20:40:34 +0530

    Create Set Matrix Zeroes.cpp

---
+++ Matrix/Set Matrix Zeroes.cpp
@@ -0,0 +1,90 @@
+//Question - https://www.geeksforgeeks.org/batch/gfg-160-problems/track/matrix-gfg-160/problem/set-matrix-zeroes
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    void setMatrixZeroes(vector<vector<int>> &mat) {
+        // code here
+        
+    int n = mat.size(), m = mat[0].size();
+
+    int c0 = 1;
+
+    // Traverse the arr and mark first cell of
+    // each row and column
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (mat[i][j] == 0) {
+
+                // mark i-th row
+                mat[i][0] = 0;
+
+                // mark j-th column
+                if (j == 0)
+                    c0 = 0;
+                else
+                    mat[0][j] = 0;
+            }
+        }
+    }
+
+    // Traverse and mark the matrix from (1, 1)
+    // to (n - 1, m - 1)
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+
+            // Check for col & row
+            if (mat[i][0] == 0 || mat[0][j] == 0) {
+                mat[i][j] = 0;
+            }
+        }
+    }
+
+    // Mark the first row
+    if (mat[0][0] == 0) {
+        for (int j = 0; j < m; j++)
+            mat[0][j] = 0;
+    }
+  
+    // Mark the first column
+    if (c0 == 0) {
+        for (int i = 0; i < n; i++)
+            mat[i][0] = 0;
+    }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int> > arr(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> arr[i][j];
+            }
+        }
+        Solution ob;
+        ob.setMatrixZeroes(arr);
+        for (auto x : arr) {
+            for (auto y : x)
+                cout << y << " ";
+            cout << endl;
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
